9a657a2181fcef248de8350b11e24b9f9c939d3a
mudassar111 <mudassartufail9@gmail.com>
2022-04-15 17:23:47 +0200

    Update model_pro.py

--- model_pro.py
+++ model_pro.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-class River:
+class River: #Optional class to input river coordinates
     def __init__(self, x1, y1, x2, y2):
         self.m = (y2-y1)/(x2-x1)
         self.theta = np.where(np.arctan(self.m)> 0, np.pi - np.arctan(self.m), -np.pi - np.arctan(self.m))
@@ -13,24 +13,24 @@ class River:
     def operator(self,x,y):
         return None
 
-class Model:
+class Model: #Model class to calculate discharge potential, head and stream function
     def __init__(self, k, H, h0, Qo_x, river = None, x_ref = 0, y_ref = 0):
         self.k = k
         self.H = H
         self.h0 = h0
         self.aem_elements = []
         self.well_df = pd.DataFrame({'wellid' : [], 'Discharge' : [], 'X' : [], 'Y' : []})
-        self.Qo_x = -Qo_x
-        self.p = 0
+        self.Qo_x = -Qo_x #Baseflow in the x-direction
+        self.p = 0 #River clogging factor
         self.x = x_ref
         self.y = y_ref
 
-        if river is None:
+        if river is None: #if not defined in river class then river is assumed at Y-axis
             self.river_a = 1
             self.river_b = 0
             self.river_c = 0
         
-        else:
+        else: #River line imported from river class
             self.river_a = river.river_a
             self.river_b = river.river_b
             self.river_c = river.river_c
@@ -40,7 +40,7 @@ class Model:
             self.phi_c = self.k * self.H * self.h0 - 0.5 * self.k * self.H**2
         self.phi0 = self.phi_c
 
-    def calc_phi(self, x, y):
+    def calc_phi(self, x, y): #Function to calculate the discharge potential
         phi_well = 0
         p = self.p
         for element in self.aem_elements:
@@ -56,7 +56,7 @@ class Model:
         phi_base = -self.Qo_x*x
         return self.phi0 + phi_well + phi_base
 
-    def calc_head(self, x, y):
+    def calc_head(self, x, y): #Function to calculate head
         phi = self.calc_phi(x,y)
         phicrit = 0.5 * self.k * self.H ** 2
         if phi >= phicrit:
@@ -65,7 +65,7 @@ class Model:
             h = np.sqrt((2 / self.k) * (phi))
         return h
 
-    def calc_psi(self, x, y):
+    def calc_psi(self, x, y): #Function to calculate stream function
         psi_well = 0
         p = self.p
         for element in self.aem_elements:
@@ -80,17 +80,17 @@ class Model:
 
         return psi
 
-    def calc_clogging(self, kd, d):
+    def calc_clogging(self, kd, d): #Calculation of clogging factor
         self.p = d*self.k/kd
         self.x = 0-self.p
         self.update_phi0()
 
-    def update_phi0(self):
+    def update_phi0(self): #Method to calculate the discharge potential at the phi0 location
         self.phi0 = 0
         phi = self.calc_phi(self.x, self.y)
         self.phi0 = self.phi_c - phi
 
-class Well:
+class Well: #Well creation class
     def __init__(self, model, Q, rw, x, y):
         self.x = x
         self.y = y
@@ -98,11 +98,5 @@ class Well:
         self.rw = rw
         model.aem_elements.append(self)
         model.update_phi0()
-
-#aem_model = Model(k = 10, H=20, h0 = 18)
-
-#well = Well(aem_model, Q = 10, rw = 0.2, x = 50, y = 50)
-
-#print(aem_model.calc_psi(120, 0))
 
 
